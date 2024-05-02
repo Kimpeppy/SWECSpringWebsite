ddd25f96d0f1ba376fe912e6ca3d0afd3e0ff508
Tanvi Ranade <tanviranade@gmail.com>
2024-05-02 12:42:32 -0400

    resized component

--- src/pages/LandingPages/AboutUs/sections/Information.js
+++ src/pages/LandingPages/AboutUs/sections/Information.js
@@ -22,14 +22,13 @@ import MKBox from "components/MKBox";
 
 // Material Kit 2 React examples
 import DefaultInfoCard from "examples/Cards/InfoCards/DefaultInfoCard";
-import CenteredBlogCard from "examples/Cards/BlogCards/CenteredBlogCard";
 
 function Information() {
   return (
     <MKBox component="section" py={12}>
       <Container>
         <Grid container spacing={3} alignItems="center">
-          <Grid item xs={12} lg={6}>
+          <Grid item xs={12} lg={30}>
             <Grid container justifyContent="flex-start">
               <Grid item xs={12} md={6}>
                 <MKBox mb={5}>
@@ -68,19 +67,6 @@ function Information() {
                 </MKBox>
               </Grid>
             </Grid>
-          </Grid>
-          <Grid item xs={12} lg={4} sx={{ ml: "auto", mt: { xs: 3, lg: 0 } }}>
-            <CenteredBlogCard
-              image="https://images.unsplash.com/photo-1544717302-de2939b7ef71?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
-              title="A snippet from our GBM"
-              description="Website visitors today demand a frictionless user expericence — especially when using search. Because of the hight standards."
-              action={{
-                type: "internal",
-                route: "pages/company/about-us",
-                color: "info",
-                label: "find out more",
-              }}
-            />
           </Grid>
         </Grid>
       </Container>
